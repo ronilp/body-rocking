9156dacbc21c751aebb96a1b4daad337c1c1a50f
Ronil-mbp <rpancho@ncsu.edu>
2019-03-22 19:00:14 -0400

    cnn model

---
+++ models/cnn_model.py
@@ -0,0 +1,27 @@
+# File: cnn_model.py
+# Author: Ronil Pancholia
+# Date: 3/23/19
+# Time: 5:52 PM
+
+from torch import nn
+
+class Cnn_Model(nn.Module):
+    def __init__(self):
+        super(Cnn_Model, self).__init__()
+        self.conv1 = nn.Conv2d(12, 24, kernel_size=(3, 1), stride=1, padding=0)
+        self.conv2 = nn.Conv2d(24, 48, kernel_size=(3, 1), stride=1, padding=0)
+        self.act = nn.ReLU()
+        self.adaptivePool = nn.AdaptiveAvgPool2d((48, 1))
+        self.fc1 = nn.Linear(2304, 1000)
+        self.fc2 = nn.Linear(1000, 2)
+        self.maxpool = nn.MaxPool2d(kernel_size=(3, 1))
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        x = x.view(-1, 12, 250, 1)
+        x = self.act(self.conv1(x))
+        x = self.act(self.conv2(x))
+        x = self.adaptivePool(x)
+        x = self.fc1(x.view(x.size(0), -1))
+        x = self.fc2(x)
+        return x
