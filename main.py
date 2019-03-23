4e1d62a8abc17122ea8fc5361db85ba3a56a8597
Ronil-mbp <rpancho@ncsu.edu>
2019-03-23 19:24:00 -0400

    save loss/accuracy after training

--- main.py
+++ main.py
@@ -2,26 +2,27 @@
 # Author: Ronil Pancholia
 # Date: 3/20/19
 # Time: 5:24 PM
+import os
 
 import numpy as np
 import torch
 from torch import nn
+import pickle
 
-from config import RANDOM_SEED, TRAIN_EPOCHS, device
+from config import RANDOM_SEED, TRAIN_EPOCHS, device, MODEL_DIR, MODEL_PREFIX
 from dataset_utils import load_datasets, load_testset
 from rocking_dataset import RockingDataset
 from training_utils import get_model, fit
 
 torch.manual_seed(RANDOM_SEED)
 
-dataset_loaders, dataset_sizes = load_datasets(RockingDataset)
-train_dataloader = dataset_loaders['train']
-val_dataloader = dataset_loaders['val']
 
-model, opt = get_model()
-criterion = nn.CrossEntropyLoss()
+def save_loss_acc(train_loss, train_acc, val_loss, val_acc):
+    pickle.dump(train_loss, open(os.path.join(MODEL_DIR, MODEL_PREFIX + "train_loss"), "wb"))
+    pickle.dump(train_acc, open(os.path.join(MODEL_DIR, MODEL_PREFIX + "train_acc"), "wb"))
+    pickle.dump(val_loss, open(os.path.join(MODEL_DIR, MODEL_PREFIX + "val_loss"), "wb"))
+    pickle.dump(val_acc, open(os.path.join(MODEL_DIR, MODEL_PREFIX + "val_acc"), "wb"))
 
-train_loss, train_acc, val_loss, val_acc = fit(TRAIN_EPOCHS, model, criterion, opt, train_dataloader, val_dataloader)
 
 def test_model():
     test_dataloader = load_testset(RockingDataset)
@@ -37,5 +38,17 @@ def test_model():
         results.extend(np.asarray(preds))
 
     print("Testing accuracy :", 100 * corrects / len(test_dataloader.dataset))
+
+
+dataset_loaders, dataset_sizes = load_datasets(RockingDataset)
+train_dataloader = dataset_loaders['train']
+val_dataloader = dataset_loaders['val']
+
+model, opt = get_model()
+criterion = nn.CrossEntropyLoss()
+
+train_loss, train_acc, val_loss, val_acc = fit(TRAIN_EPOCHS, model, criterion, opt, train_dataloader, val_dataloader)
+
+save_loss_acc(train_loss, train_acc, val_loss, val_acc)
 
 test_model()
